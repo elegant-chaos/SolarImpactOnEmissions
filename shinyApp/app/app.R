61852af0b451fab8bc07b05c9ed486290fa40a1e
Halbleib <mm06682@MM06682M1.local>
2019-07-31 12:29:29 -0400

    Adding placeholder line plot for emissions output

--- shinyApp/app/app.R
+++ shinyApp/app/app.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(plotly)
 library(googlesheets)
 
 table <- "test_ddfg"
@@ -44,7 +45,7 @@ shinyApp(
         ),
         mainPanel(
             tabsetPanel(type = "tabs",
-                        tabPanel("Placeholder 1"),
+                        tabPanel("Placeholder 1", plotlyOutput("plot1")),
                         tabPanel("Placeholder 2"),
                         tabPanel("Data Table", DT::dataTableOutput("responses", width = 300), tags$hr())
         ))
@@ -67,7 +68,28 @@ shinyApp(
         output$responses <- DT::renderDataTable({
             input$submit
             loadData()
-        })     
+        })
+        
+        # The following plot is a placeholder
+        # Can use a line plot of this type to show the emissions savings for 
+        # all 3 emissions types listed in the eGrid data over time.
+        # X axis -> month
+        # Y axis -> emissions
+        # color -> emission type (carbon, etc.)
+        # (Above instructions just one idea. I'm sure many good ideas exist in this space.)
+        
+        trace_0 <- rnorm(100, mean = 5)
+        trace_1 <- rnorm(100, mean = 0)
+        trace_2 <- rnorm(100, mean = -5)
+        x <- c(1:100)
+        
+        data <- data.frame(x, trace_0, trace_1, trace_2)
+        
+        output$plot1 <- renderPlotly({
+            plot_ly(data, x = ~x, y = ~trace_0, name = 'trace 0', type = 'scatter', mode = 'lines') %>%
+            add_trace(y = ~trace_1, name = 'trace 1', mode = 'lines+markers') %>%
+            add_trace(y = ~trace_2, name = 'trace 2', mode = 'markers')
+        })
     }
 )
 
